5a999f0b99ba3d782a55e25fd29310d058d5682f
zhangxinxia <expelliarmus@teamsz.xyz>
2017-09-03 17:13:43 +0800

    chatting

--- app/src/main/java/com/example/a111/myapplication/DoctorAdapter.java
+++ app/src/main/java/com/example/a111/myapplication/DoctorAdapter.java
@@ -2,6 +2,7 @@ package com.example.a111.myapplication;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -20,11 +22,12 @@ public class DoctorAdapter extends RecyclerView.Adapter<DoctorAdapter.ViewHolder
     private Context mContext;
     private List<Doctor> doctorList;
     static class ViewHolder extends RecyclerView.ViewHolder{
-        CardView cardView;
+        View doctorView;
         TextView doctorstat;
         TextView doctorname;
         public ViewHolder(View view) {
             super(view);
+            doctorView=view;
             doctorstat=(TextView)view.findViewById(R.id.id_status);
             doctorname=(TextView)view.findViewById(R.id.id_doctors);
         }
@@ -34,13 +37,26 @@ public class DoctorAdapter extends RecyclerView.Adapter<DoctorAdapter.ViewHolder
         this.doctorList = doctorList;
     }
 
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (mContext==null){
             mContext=parent.getContext();
         }
         View view= LayoutInflater.from(mContext).inflate(R.layout.chat_list,parent,false);
-        return new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.doctorView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                int positon = holder.getAdapterPosition();
+                Doctor doctor = doctorList.get(positon);
+                Toast.makeText(v.getContext(),"向"+doctor.getName()+"咨询",Toast.LENGTH_SHORT).show();
+                Intent asking = new Intent(mContext,AskActivity.class);
+                mContext.startActivity(asking);
+            }
+        });
+        return holder;
     }
 
     @Override
